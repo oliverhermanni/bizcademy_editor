b05472f9b23342b6c988a141a0819e8ac164a750
Oliver Hermanni <info@oliver-hermanni.de>
2013-11-12 16:37:41 +0100

    Initial commit
    Basic setup of bizcademy editor

---
+++ server.js
@@ -0,0 +1,18 @@
+// setup server ===
+var express = require('express');
+var mongoose = require('mongoose');
+var editor = new express();
+
+editor.configure(function() {
+    editor.use(express.static(__dirname + "/public"));
+    editor.use(express.logger('dev'));
+    editor.use(express.bodyParser());
+    editor.use(express.methodOverride());
+})
+
+editor.get('*', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+editor.listen(8080);
+console.log("App listening on port 8080");
