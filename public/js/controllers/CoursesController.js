ca5af42ff31b1bcfb3a63fef95d13e8553e2862a
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-09 12:16:00 +0100

    Moved getting chapters from root to here

--- public/js/controllers/CoursesController.js
+++ public/js/controllers/CoursesController.js
@@ -1,5 +1,5 @@
 CourseEditor.controller('CoursesController',
-    function ($scope, $location, $routeParams) {
-
+    function ($scope, $location, $routeParams, ChapterModel) {
+        $scope.chapters = ChapterModel.getChapters($scope.courseId);
     }
 );
