f841f906eaf2fe118660755a7f1c0b2f9938575d
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-10 18:40:51 +0100

    Added function to display single chapter

--- public/js/models/ChapterModel.js
+++ public/js/models/ChapterModel.js
@@ -1,66 +1,63 @@
-CourseEditor.service('ChapterModel', function() {
+CourseEditor.service('ChapterModel', function () {
 
-    this.getChapters = function(courseId) {
+  this.getChapters = function (courseId) {
 
-        var course = JSON.parse(window.localStorage.getItem(courseId));
+    var course = JSON.parse(window.localStorage.getItem(courseId));
 
-        if (!course) {
-            return []
-        } else {
-            return course.chapters;
-        }
-    };
+    if (!course) {
+      return []
+    } else {
+      return course.chapters;
+    }
+  };
 
-    this.getSingleChapter = function (courseId, chapterId) {
-        var course = JSON.parse(window.localStorage.getItem(courseId));
+  this.getChapterById = function (courseId, chapterId) {
+    var course = JSON.parse(window.localStorage.getItem(courseId));
 
-        if (!course) {
-            return []
-        }
-
-        for (var i = 0; i < course.chapters.length; i++) {
-            if (course.chapters[i].id === chapterId) {
-                return course.chapters[i];
-            }
-        }
-
-
-        return chapter;
+    if (!course) {
+      return []
     }
 
-    this.addChapter = function(courseId, chapterTitle, chapterContent) {
-        var chapter = {
-            title: chapterTitle,
-            content: chapterContent,
-            id: Math.random().toString(36).substring(7)
-        }
+    for (var i = 0; i < course.chapters.length; i++) {
+      if (course.chapters[i].id === chapterId) {
+        return course.chapters[i];
+      }
+    }
+  }
 
-        var course = JSON.parse(window.localStorage.getItem(courseId));
+  this.addChapter = function (courseId, chapterTitle, chapterContent) {
+    var chapter = {
+      title: chapterTitle,
+      content: chapterContent,
+      id: Math.random().toString(36).substring(7)
+    }
 
-        if (!course) {
-            course = {
-                id: courseId,
-                chapters: []
-            }
-        }
-        course.chapters.push(chapter);
+    var course = JSON.parse(window.localStorage.getItem(courseId));
+
+    if (!course) {
+      course = {
+        id: courseId,
+        chapters: []
+      }
+    }
+    course.chapters.push(chapter);
+    window.localStorage.setItem(courseId, JSON.stringify(course));
+  };
+
+  this.deleteChapter = function (courseId, chapterId) {
+    var course = JSON.parse(window.localStorage.getItem(courseId));
+    console.log('test');
+
+    if (!course || !course.chapters) {
+      return;
+    }
+    for (var i = 0; i < course.chapters.length; i++) {
+      if (course.chapters[i].id === chapterId) {
+        course.chapters.splice(i, 1);
         window.localStorage.setItem(courseId, JSON.stringify(course));
-    };
-
-    this.deleteChapter = function(courseId, chapterId) {
-        var course = JSON.parse(window.localStorage.getItem(courseId));
-        console.log('test');
-
-        if (!course || !course.chapters) {
-            return;
-        }
-        for (var i=0; i < course.chapters.length; i++) {
-            if (course.chapters[i].id === chapterId) {
-                course.chapters.splice(i, 1);
-                window.localStorage.setItem(courseId, JSON.stringify(course));
-                break;
-            }
-        }
+        break;
+      }
     }
+  }
 
 });
