6df8d08ba81ed0fc84c14bc6be3af3c352a87a0b
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-10 18:44:59 +0100

    Show single chapter

--- public/js/controllers/ChaptersController.js
+++ public/js/controllers/ChaptersController.js
@@ -1,35 +1,29 @@
 CourseEditor.controller('ChaptersController',
-    function ($scope, $location, $routeParams, ChapterModel) {
-      $scope.chapter = ChapterModel.getChapter($scope.chapterId);
-    }
-);
-
-CourseEditor.controller('ChaptersController',
-    function ($scope, $location, $routeParams, ChapterModel) {
-      $scope.chapters = ChapterModel.getChapters($scope.courseId);
-    }
+  function ($scope, $location, $routeParams, ChapterModel) {
+    $scope.chapter = ChapterModel.getChapterById($scope.courseId, $routeParams.chapterId);
+  }
 );
 
 CourseEditor.controller('ChapterAddController',
-    function ($scope, $location, $routeParams, ChapterModel) {
+  function ($scope, $location, $routeParams, ChapterModel) {
 
-        $scope.cancel = function() {
-            $location.path('/');
-        }
-
-        $scope.createChapter = function() {
-
-            ChapterModel.addChapter($scope.courseId, $scope.chapter.title, $scope.chapter.content);
-            $location.path('/');
-        }
+    $scope.cancel = function () {
+      $location.path('/');
     }
+
+    $scope.createChapter = function () {
+
+      ChapterModel.addChapter($scope.courseId, $scope.chapter.title, $scope.chapter.content);
+      $location.path('/');
+    }
+  }
 );
 
 CourseEditor.controller('ChapterDeleteController',
-    function ($scope, $location, $routeParams, ChapterModel) {
-        var chapterId = $routeParams.chapterId;
+  function ($scope, $location, $routeParams, ChapterModel) {
+    var chapterId = $routeParams.chapterId;
 
-        ChapterModel.deleteChapter($scope.courseId, chapterId);
-        $location.path('/');
-    }
+    ChapterModel.deleteChapter($scope.courseId, chapterId);
+    $location.path('/');
+  }
 );
