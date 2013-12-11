d8738b3e2e1b1e871780351cefabda076a88c0c8
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-11 13:05:48 +0100

    Added route for course show

--- public/js/courseeditor.js
+++ public/js/courseeditor.js
@@ -1,29 +1,37 @@
-var courseEditorConfig = function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            controller: 'CoursesController',
-            templateUrl: 'js/views/course/show.html'
-        })
-        .when('/chapter/:chapterId', {
-            controller: 'ChaptersController',
-            templateUrl: 'js/views/chapter/show.html'
-        })
-        .when('/addchapter', {
-            controller: 'ChapterAddController',
-            templateUrl: 'js/views/chapter/edit.html'
-        })
-        .when('/deletechapter/:chapterId', {
-            controller: 'ChapterDeleteController',
-            templateUrl: 'js/views/course/show.html'
-         })
-        .otherwise({
-            redirectTo:'/'
-        });
+var courseEditorConfig = function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'CoursesController',
+      templateUrl: 'js/views/course/index.html'
+    })
+    .when('/course/add', {
+      controller: 'CourseAddController',
+      templateUrl: 'js/views/course/edit.html'
+    })
+    .when('/course/:courseId', {
+      controller: 'CoursesController',
+      templateUrl: 'js/views/course/show.html'
+    })
+    .when('/chapter/:chapterId', {
+      controller: 'ChaptersController',
+      templateUrl: 'js/views/chapter/show.html'
+    })
+    .when('/addchapter', {
+      controller: 'ChapterAddController',
+      templateUrl: 'js/views/chapter/edit.html'
+    })
+    .when('/deletechapter/:chapterId', {
+      controller: 'ChapterDeleteController',
+      templateUrl: 'js/views/course/show.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 };
 
 
 var CourseEditor = angular.module('CourseEditor', ['ngRoute'])
-    .config(courseEditorConfig)
-    .run(function($rootScope) {
-        $rootScope.courseId = "999";
-    });
+  .config(courseEditorConfig)
+  .run(function ($rootScope) {
+    $rootScope.courseId = "999";
+  });
