fb1baef89489b1b90accc21f4ad4d330594c216b
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-11 11:45:19 +0100

    Removed all chapter relevant code from CoursesController

--- public/js/controllers/CoursesController.js
+++ public/js/controllers/CoursesController.js
@@ -1,5 +1,4 @@
 CourseEditor.controller('CoursesController',
-    function ($scope, $location, $routeParams, ChapterModel) {
-        $scope.chapters = ChapterModel.getChapters($scope.courseId);
+    function ($scope, $location, $routeParams) {
     }
 );
