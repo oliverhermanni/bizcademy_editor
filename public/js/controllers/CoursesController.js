599ae1afb959122add015563b17813ea5aaf44c1
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-11 13:05:14 +0100

    CoursesAddController added

--- public/js/controllers/CoursesController.js
+++ public/js/controllers/CoursesController.js
@@ -1,4 +1,21 @@
 CourseEditor.controller('CoursesController',
-    function ($scope, $location, $routeParams) {
+  function ($scope, $location, $routeParams, CourseModel) {
+    $scope.courses = CourseModel.getCourses();
+  }
+);
+
+CourseEditor.controller('CourseAddController',
+  function ($scope, $location, $routeParams, CourseModel) {
+
+    $scope.currentTask = "Add";
+
+    $scope.cancel = function () {
+      $location.path('/');
     }
+
+    $scope.createCourse = function () {
+      CourseModel.addCourse($scope.course);
+      $location.path('/');
+    }
+  }
 );
