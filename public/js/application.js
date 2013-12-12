c02bccb0de84a3677f38c9ecd03995b5f1e64e8b
Gregor Swatek <gregor.swatek@gmail.com>
2013-12-12 08:14:01 +0100

    chaged app js

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,11 @@
 $(function() {
   $(".custom-checkbox label").click(function() {
     var checkbox = $(this).prev("input");
-
+    console.log(checkbox);
     if (checkbox.attr("checked") === "checked") {
-      checkbox.attr("checked",false);
+      checkbox.attr("checked","");
     } else {
-      checkbox.attr("checked",true);
+      checkbox.attr("checked","checked");
     }
   });
 });
