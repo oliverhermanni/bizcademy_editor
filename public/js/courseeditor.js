92d3b06851da5769b05a99a1b6c5b522c81feeec
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-13 15:44:07 +0100

    Added richtexteditor directive

--- public/js/courseeditor.js
+++ public/js/courseeditor.js
@@ -24,6 +24,10 @@ var courseEditorConfig = function($routeProvider) {
         controller: 'ChaptersController',
         templateUrl: 'js/views/chapter/show.html'
       })
+      .when('/chapter/:chapterId/module/text/add', {
+        controller: 'TextModuleController',
+        templateUrl: 'js/views/modules/text/edit.html'
+      })
       .when('/quiz', {
         controller: 'QuizController',
         templateUrl: 'js/views/quiz/show.html'
@@ -38,3 +42,54 @@ var CourseEditor = angular.module('CourseEditor', ['ngRoute'])
     .config(courseEditorConfig)
     .run(function($rootScope) {
     });
+
+CourseEditor.directive('richTextEditor', function( $log, $location ) {
+
+  var self = this;
+  var directive = {
+    restrict : "A",
+    replace : true,
+    transclude : true,
+    scope : {
+
+    },
+    template :
+
+      "<div>" +
+
+        "<textarea id=\"richtexteditor-content\" style=\"height:300px;width:100%\"></textarea>"+
+
+        "</div>",
+
+    link : function( $scope, $element, $attrs ) {
+      $scope.editor = $('#richtexteditor-content').wysihtml5();
+
+
+//                        $scope.editor = new wysihtml5.Editor( "richtexteditor-content", {
+
+     //                           toolbar : "richtexteditor-toolbar",
+      //                              parserRules: wysihtml5ParserRules
+      //                    });
+
+      $scope.$parent.$watch( $attrs.content, function( newValue, oldValue ) {
+
+        $scope.editor.innerHTML = newValue;
+        $scope.editor.composer.setValue( newValue );
+      });
+
+      $scope.cancel = function() {
+        $scope.$parent.cancel();
+      }
+      /* $scope.save = function() {
+       var currentTemplateContent = $encryption.encodeHtml( $scope.editor.getValue() );
+       $scope.$parent.currentTemplate.content = currentTemplateContent;
+       $scope.$parent.save();
+       }
+       */
+      $scope.isClean = function() {
+        $scope.$parent.isClean();
+      }
+    }
+  }
+  return directive;
+});
