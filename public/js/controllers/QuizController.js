5919e19bcbb8a874b1fec87f2257c5fb1a8e9b36
Gregor Swatek <gregor.swatek@gmail.com>
2013-12-16 11:44:35 +0100

    try to fix checkbox js, moved it to controller in case of dom ready fuckup, removed useless applicationJS

--- public/js/controllers/QuizController.js
+++ public/js/controllers/QuizController.js
@@ -1,4 +1,17 @@
 CourseEditor.controller('QuizController',
   function ($scope, $location, $routeParams) {
+    $scope.$on('$viewContentLoaded', function(){
+      $(".custom-checkbox").click(function() {
+        var checkbox = $(this).prev();
+
+        if (checkbox.is(":checked")) {
+          checkbox.removeAttr("checked");
+          $(this).removeClass("checked");
+        } else {
+          checkbox.attr("checked","checked");
+          $(this).addClass("checked");
+        }
+      });
+    });
   }
 );
