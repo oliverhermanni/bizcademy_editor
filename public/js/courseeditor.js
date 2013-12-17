5dfb43a7d7b9d816c405727c960bd786c2de77bf
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-17 11:20:28 +0100

    Added model to textarea

--- public/js/courseeditor.js
+++ public/js/courseeditor.js
@@ -1,92 +1,77 @@
-var courseEditorConfig = function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        controller: 'CoursesController',
-        templateUrl: 'js/views/course/index.html'
-      })
-      .when('/course/add', {
-        controller: 'CourseAddController',
-        templateUrl: 'js/views/course/edit.html'
-      })
-      .when('/course/:courseId', {
-        controller: 'CoursesController',
-        templateUrl: 'js/views/course/show.html'
-      })
-      .when('/course/:courseId/chapter/add', {
-        controller: 'ChapterAddController',
-        templateUrl: 'js/views/chapter/edit.html'
-      })
-      .when('/course/:courseId/chapter/delete/:chapterId', {
-        controller: 'ChapterDeleteController',
-        templateUrl: 'js/views/course/show.html'
-      })
-      .when('/course/:courseId/chapter/:chapterId', {
-        controller: 'ChaptersController',
-        templateUrl: 'js/views/chapter/show.html'
-      })
-      .when('/chapter/:chapterId/module/text/add', {
-        controller: 'TextModuleController',
-        templateUrl: 'js/views/modules/text/edit.html'
-      })
-      .when('/quiz', {
-        controller: 'QuizController',
-        templateUrl: 'js/views/quiz/show.html'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+var courseEditorConfig = function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'CoursesController',
+      templateUrl: 'js/views/course/index.html'
+    })
+    .when('/course/add', {
+      controller: 'CourseAddController',
+      templateUrl: 'js/views/course/edit.html'
+    })
+    .when('/course/:courseId', {
+      controller: 'CoursesController',
+      templateUrl: 'js/views/course/show.html'
+    })
+    .when('/course/:courseId/chapter/add', {
+      controller: 'ChapterAddController',
+      templateUrl: 'js/views/chapter/edit.html'
+    })
+    .when('/course/:courseId/chapter/delete/:chapterId', {
+      controller: 'ChapterDeleteController',
+      templateUrl: 'js/views/course/show.html'
+    })
+    .when('/course/:courseId/chapter/:chapterId/module/text/add', {
+      controller: 'TextModuleAddController',
+      templateUrl: 'js/views/modules/text/edit.html'
+    })
+    .when('/course/:courseId/chapter/:chapterId', {
+      controller: 'ChaptersController',
+      templateUrl: 'js/views/chapter/show.html'
+    })
+    .when('/quiz', {
+      controller: 'QuizController',
+      templateUrl: 'js/views/quiz/show.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 };
 
 
 var CourseEditor = angular.module('CourseEditor', ['ngRoute'])
-    .config(courseEditorConfig)
-    .run(function($rootScope) {
-    });
+  .config(courseEditorConfig)
+  .run(function ($rootScope) {
+  });
 
-CourseEditor.directive('richTextEditor', function( $log, $location ) {
+CourseEditor.directive('richTextEditor', function ($log, $location) {
 
   var self = this;
   var directive = {
-    restrict : "A",
-    replace : true,
-    transclude : true,
-    scope : {
+    restrict: "A",
+    replace: true,
+    transclude: true,
+    scope: {
 
     },
-    template :
+    template: "<div>" +
 
-      "<div>" +
+      "<textarea id=\"richtexteditor-content\" style=\"height:300px;width:100%\" ng-model=\"textmodule.text\"></textarea>" +
 
-        "<textarea id=\"richtexteditor-content\" style=\"height:300px;width:100%\"></textarea>"+
+      "</div>",
 
-        "</div>",
-
-    link : function( $scope, $element, $attrs ) {
+    link: function ($scope, $element, $attrs) {
       $scope.editor = $('#richtexteditor-content').wysihtml5();
 
-
-//                        $scope.editor = new wysihtml5.Editor( "richtexteditor-content", {
-
-     //                           toolbar : "richtexteditor-toolbar",
-      //                              parserRules: wysihtml5ParserRules
-      //                    });
-
-      $scope.$parent.$watch( $attrs.content, function( newValue, oldValue ) {
+      $scope.$parent.$watch($attrs.content, function (newValue, oldValue) {
 
         $scope.editor.innerHTML = newValue;
-        $scope.editor.composer.setValue( newValue );
+        $scope.editor.composer.setValue(newValue);
       });
 
-      $scope.cancel = function() {
+      $scope.cancel = function () {
         $scope.$parent.cancel();
       }
-      /* $scope.save = function() {
-       var currentTemplateContent = $encryption.encodeHtml( $scope.editor.getValue() );
-       $scope.$parent.currentTemplate.content = currentTemplateContent;
-       $scope.$parent.save();
-       }
-       */
-      $scope.isClean = function() {
+      $scope.isClean = function () {
         $scope.$parent.isClean();
       }
     }
