a48affa028da22b85e29e39a2545e431485591c1
Gregor Swatek <gregor.swatek@gmail.com>
2013-12-18 23:19:12 +0100

    added progressbar for chapter steps and point counting animation

--- public/js/controllers/QuizController.js
+++ public/js/controllers/QuizController.js
@@ -1,6 +1,15 @@
 CourseEditor.controller('QuizController',
   function ($scope, $location, $routeParams) {
     $scope.$on('$viewContentLoaded', function(){
+      $("button[data-toggle='pointUp']").click(function() {
+        $(".total-point-count").toggleClass("point-up");
+        if ($(".progress").hasClass("active")) {
+          $(".progress-bar").css("width","50%");
+        } else {
+          $(".progress-bar").css("width","65%");
+        }
+        $(".progress").toggleClass("progress-striped active");
+      });
       $(".custom-checkbox").click(function() {
         var checkbox = $(this).prev();
 
