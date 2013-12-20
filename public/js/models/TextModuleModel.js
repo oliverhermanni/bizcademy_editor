3f2e588268819315c18dcb8c4afb0e38d0a7f97d
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-20 16:53:58 +0100

    Removed modules key from array

--- public/js/models/TextModuleModel.js
+++ public/js/models/TextModuleModel.js
@@ -1,20 +1,19 @@
 CourseEditor.service('TextModuleModel', function() {
   this.addTextModule = function(chapterId, moduleData) {
-    var current_chapter = window.localStorage.getItem(chapterId);
+    var current_chapter = JSON.parse(window.localStorage.getItem(chapterId));
 
     if (!current_chapter) {
-      current_chapter = {
-        modules: []
-      }
+      current_chapter = []
     }
 
     var textModule = {
       id: Math.random().toString(36).substring(7),
+      type: 'text',
       title: moduleData.title,
       text: moduleData.text
     }
 
-    current_chapter.modules.push(textModule);
+    current_chapter.push(textModule);
 
     window.localStorage.setItem(chapterId, JSON.stringify(current_chapter));
 
