ad776381f2a13cc6133996bca129be70e8fecf81
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-28 21:23:05 +0100

    Fixed saving of QuizModule

--- public/js/controllers/QuizModuleController.js
+++ public/js/controllers/QuizModuleController.js
@@ -28,41 +28,38 @@ CourseEditor.controller('QuizModuleAddController',
     }
 
     $scope.addQuizModule = function () {
-      alert('test');
 
-
-      var answers = [];
-
-      answers[0] = {
-        answer: $scope.quizmodule.answer1
-      };
-      answers[1] = {
-        answer: $scope.quizmodule.answer2
-      };
-      answers[2] = {
-        answer: $scope.quizmodule.answer3
-      };
-      answers[3] = {
-        answer: $scope.quizmodule.answer4
+      var answers = {
+        0: {
+          answer: $scope.quizmodule.answer1
+        },
+        0: {
+          answer: $scope.quizmodule.answer2
+        },
+        0: {
+          answer: $scope.quizmodule.answer3
+        },
+        0: {
+          answer: $scope.quizmodule.answer4
+        }
       };
 
+      var hints = {
+        0: {
+          hint: $scope.quizmodule.hint1
+        },
+        1: {
+          hint: $scope.quizmodule.hint1
+        },
+        2: {
+          hint: $scope.quizmodule.hint1
+        },
+        3: {
+          hint: $scope.quizmodule.hint1
+        }
+      }
 
-      var hints = [];
-
-      hints[0] = {
-        hint: $scope.quizmodule.hint1
-      };
-      hints[1] = {
-        hint: $scope.quizmodule.hint2
-      };
-      hints[2] = {
-        hint: $scope.quizmodule.hint3
-      };
-
-      $scope.quizmodule.push(answers);
-      $scope.quizmodule.push(hints);
-
-      QuizModuleModel.addQuizModule($routeParams.chapterId, $scope.quizmodule, editor.html());
+      QuizModuleModel.addQuizModule($routeParams.chapterId, $scope.quizmodule, editor.getValue(), answers, hints);
       $location.path('/course/' + $routeParams.courseId + '/chapter/' + $routeParams.chapterId);
     }
   }
