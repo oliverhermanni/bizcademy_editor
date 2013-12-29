7b4608734e8ec1704692623eead0c7fe9d92edf2
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-29 18:14:55 +0100

    Added to_trusted function

--- public/js/controllers/CoursesController.js
+++ public/js/controllers/CoursesController.js
@@ -1,14 +1,15 @@
 CourseEditor.controller('CoursesController',
   function ($scope, $location, $routeParams, CourseModel, ChapterModel, $sce) {
+
+    // TODO: make this a global function
+    $scope.to_trusted = function()  {
+      return $sce.trustAsHtml($scope.course.summary);
+    }
+
     $scope.courses = CourseModel.getCourses();
 
     if ($routeParams.courseId) {
       $scope.course = CourseModel.getCourseById($routeParams.courseId);
-    }
-
-    // TODO: make this a global function
-    $scope.to_trusted = function()  {
-      return $sce.trustAsHtml($scope.chapter.summary);
     }
 
   }
@@ -24,8 +25,7 @@ CourseEditor.controller('CourseAddController',
     }
 
     $scope.createCourse = function () {
-      var course_id = CourseModel.addCourse($scope.course);
-      alert(course_id);
+      var course_id = CourseModel.addCourse($scope.course, editor.getValue());
       $location.path('/course/' + course_id);
     }
   }
