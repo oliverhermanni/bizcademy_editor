f7b19b451551f886566a98696c9412f3ebfb56ac
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-29 14:47:14 +0100

    Fixed checkbox

--- public/js/controllers/QuizModuleController.js
+++ public/js/controllers/QuizModuleController.js
@@ -1,26 +1,19 @@
 CourseEditor.controller('QuizModuleController',
   function ($scope, $location, $routeParams) {
-
-
-    $scope.$on('$viewContentLoaded', function(){
-      $(".custom-checkbox").click(function() {
-        var checkbox = $(this).prev();
-
-        if (checkbox.is(":checked")) {
-          checkbox.removeAttr("checked");
-          $(this).removeClass("checked");
-        } else {
-          checkbox.attr("checked","checked");
-          $(this).addClass("checked");
-        }
-      });
-    });
-
   }
 );
 
 CourseEditor.controller('QuizModuleAddController',
   function ($scope, $location, $routeParams, QuizModuleModel) {
+
+    $scope.$on('$viewContentLoaded', function(){
+      $(".custom-checkbox-control").click(function() {
+        var checkbox = $(this).children('input:checkbox');
+        checkbox.attr("checked", checkbox.is(":checked"));
+      });
+    });
+
+
     $scope.currentTask = "hinzufügen";
 
     $scope.cancel = function () {
@@ -53,9 +46,6 @@ CourseEditor.controller('QuizModuleAddController',
         },
         2: {
           hint: $scope.quizmodule.hint3
-        },
-        3: {
-          hint: $scope.quizmodule.hint4
         }
       }
 
