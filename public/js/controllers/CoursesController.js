bce50a8642b500683fc7a597e5f8183fec40056e
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-29 18:36:43 +0100

    Removed to_trusted

--- public/js/controllers/CoursesController.js
+++ public/js/controllers/CoursesController.js
@@ -1,11 +1,6 @@
 CourseEditor.controller('CoursesController',
   function ($scope, $location, $routeParams, CourseModel, ChapterModel, $sce) {
 
-    // TODO: make this a global function
-    $scope.to_trusted = function()  {
-      return $sce.trustAsHtml($scope.course.summary);
-    }
-
     $scope.courses = CourseModel.getCourses();
 
     if ($routeParams.courseId) {
