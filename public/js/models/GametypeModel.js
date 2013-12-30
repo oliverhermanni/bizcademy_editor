42fa3427217927445763607d54b26a9c0208e61b
Oliver Hermanni <info@oliver-hermanni.de>
2013-12-30 03:27:58 +0100

    Not working! Basic Gametype Model

--- public/js/models/GametypeModel.js
+++ public/js/models/GametypeModel.js
@@ -1,3 +1,18 @@
-CourseEditor.service('GametypeModel', function() {
+CourseEditor.service('GametypeModel', function(CourseModel, ChapterModel) {
+  this.getModelById = function (courseId, chapterId, moduleId) {
+    alert('test');
+
+    var chapter = ChapterModel.getChapterById(courseId, chapterId)
+
+    if (!chapter) {
+      return []
+    }
+
+    for (var i = 0; i < chapter.myCourses.length; i++) {
+      if (chapter.myCourses[i].id === moduleId) {
+        return chapter.myCourses[i];
+      }
+    }
+  }
 
 });
