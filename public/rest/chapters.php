54200cfca863eea2d895edc9253395e4338054e4
oliverhermanni <oliver@hermanni.info>
2014-01-16 16:55:08 +0100

    API calls for chapters

---
+++ public/rest/chapters.php
@@ -0,0 +1,47 @@
+<?php
+$f3->route('POST /addchapter', function($f3) {
+    $request = $f3->get('BODY');
+    $chapter = json_decode($request);
+    $sql = "INSERT INTO chapters (course_id, title, summary, advice) VALUES (:course_id, :title, :summary, :advice)";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("course_id", $chapter->course_id);
+        $stmt->bindParam("title", $chapter->title);
+        $stmt->bindParam("summary", $chapter->summary);
+        $stmt->bindParam("advice", $chapter->advice);
+        $stmt->execute();
+        $chapter->id = $db->lastInsertId();
+        $db = null;
+        echo json_encode($chapter);
+    } catch (PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$f3->route('GET /getchapters/@course_id', function($f3, $params) {
+    $sql = "SELECT * FROM `chapters` WHERE `course_id` = :course_id ";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->execute(array(':course_id' => $params['course_id']));
+        $chapters = $stmt->fetchAll();
+        echo json_encode($chapters);
+    } catch (PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$f3->route('GET /getchapter/@chapterid', function($f3, $params) {
+
+    $sql = "SELECT * FROM `chapters` WHERE `id` = :id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->execute(array(':id' => $params['chapterid']));
+        $chapter = $stmt->fetch();
+        echo json_encode($chapter);
+    } catch (PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
