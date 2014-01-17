100228e72d27afbbd3574e7b7383729f0994ee6a
oliverhermanni <oliver@hermanni.info>
2014-01-17 19:01:07 +0100

    Rest API

---
+++ public/rest/index.php
@@ -0,0 +1,18 @@
+<?php
+$f3 = require('lib/base.php');
+include_once('courses.php');
+include_once('chapters.php');
+include_once('modules.php');
+
+function getConnection() {
+    $dbhost="127.0.0.1";
+    $dbuser="root";
+    $dbpass="";
+    $dbname="bizcademy";
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+}
+
+$f3->run();
+
