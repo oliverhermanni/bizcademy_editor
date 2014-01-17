a4cfc22a7319867c6b0dc284b1b8a1282e50ff2d
oliverhermanni <oliver@hermanni.info>
2014-01-17 19:00:11 +0100

    API calls for chapters

--- public/rest/chapters.php
+++ public/rest/chapters.php
@@ -26,6 +26,10 @@ $f3->route('GET /getchapters/@course_id', function($f3, $params) {
         $stmt = $db->prepare($sql);
         $stmt->execute(array(':course_id' => $params['course_id']));
         $chapters = $stmt->fetchAll();
+        for($i = 0; $i < count($chapters); $i++) {
+
+             $chapters[$i]["modules"] = getModulesOfChapter($chapters[$i]["id"]);
+        }
         echo json_encode($chapters);
     } catch (PDOException $e) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
@@ -45,3 +49,28 @@ $f3->route('GET /getchapter/@chapter_id', function($f3, $params) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
 });
+
+$f3->route('DELETE /deletechapter/@chapter_id', function($f3, $params) {
+    $sql = "DELETE FROM `chapters` WHERE `id` = :id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $count = $stmt->execute(array(':id' => $params['chapter_id']));
+        echo json_encode($count);
+    } catch (PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+function getModulesOfChapter($chapterId) {
+    $sql = "SELECT * FROM `modules` WHERE `chapter_id` = :chapter_id ";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->execute(array(':chapter_id' => $chapterId));
+        $modules = $stmt->fetchAll();
+        return $modules;
+    } catch (PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
