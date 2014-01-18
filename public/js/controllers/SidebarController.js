f12e91723634b1e2f5ddd5e1eaa9df3261809144
oliverhermanni <oliver@hermanni.info>
2014-01-18 12:09:01 +0100

    Added rest calls to Sidebar

--- public/js/controllers/SidebarController.js
+++ public/js/controllers/SidebarController.js
@@ -1,5 +1,25 @@
 CourseEditor.controller('SidebarController',
-  function ($scope, $location, $routeParams, CourseModel) {
-    $scope.course = CourseModel.getCourseById($routeParams.courseId);
+  function ($scope, $http, $location, $routeParams, CourseModel) {
+    if ($routeParams.courseId) {
+      $http.get('/rest/getcourse/' + $routeParams.courseId)
+        .success(function(data) {
+          $scope.course = data;
+        })
+        .error(function(data) {
+          alert(data);
+        });
+    }
+
+    if ($routeParams.chapterId) {
+      $http.get('/rest/getchapters/' + $routeParams.courseId)
+        .success(function(data) {
+          $scope.chapters = data;
+        })
+        .error(function(data) {
+          alert(data);
+        });
+    }
+
+
   }
 );
