91ae07e77d92c857c6e5c7e46a0b4614843652e8
oliverhermanni <oliver@hermanni.info>
2014-01-18 12:09:47 +0100

    Added Edit controller

--- public/js/controllers/TextModuleController.js
+++ public/js/controllers/TextModuleController.js
@@ -17,13 +17,54 @@ CourseEditor.controller('TextModuleAddController',
     $scope.createTextModule = function () {
 
       var textModule = {
-        chapter_id: $routeParams['courseId'],
+        chapter_id: $routeParams['chapterId'],
         module_type: 'text',
         title: $scope.textmodule.title,
         summary:  $('.note-editable').html()
       };
 
       $http.post('/rest/addmodule', textModule)
+        .success(function ($data) {
+          $location.path('/course/' + $routeParams.courseId + '/chapter/' + $routeParams.chapterId);
+        })
+        .error(function ($data) {
+          alert($data);
+        }
+      );
+    }
+  }
+);
+
+CourseEditor.controller('TextModuleEditController',
+  function ($scope, $http, $location, $routeParams, TextModuleModel) {
+    $scope.currentTask = "bearbeiten";
+
+    if ($routeParams.moduleId) {
+      $http.get('/rest/getmodule/' + $routeParams.moduleId)
+        .success(function(data) {
+          $('.note-editable').html(data.summary)
+          $scope.textmodule = data;
+
+        })
+        .error(function(data) {
+          alert(data);
+        });
+    }
+
+    $scope.cancel = function () {
+      $location.path('/course/' + $routeParams.courseId + '/chapter/' + $routeParams.chapterId);
+    }
+
+    $scope.createTextModule = function () {
+
+      var textModule = {
+        chapter_id: $routeParams['chapterId'],
+        module_type: 'text',
+        title: $scope.textmodule.title,
+        summary:  $('.note-editable').html()
+      };
+
+      $http.post('/rest/updatemodule/' + $routeParams['moduleId'], textModule)
         .success(function ($data) {
           $location.path('/course/' + $routeParams.courseId + '/chapter/' + $routeParams.chapterId);
         })
