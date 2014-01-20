e305eb3d25aa7846aa1dba1847f20633d307d102
Gregor Swatek <gregor.swatek@gmail.com>
2014-01-20 18:15:04 +0100

    finished user profiling styles

--- public/js/controllers/ProfilingController.js
+++ public/js/controllers/ProfilingController.js
@@ -1,11 +1,11 @@
 CourseEditor.controller('ProfilingController',
   function ($scope, $location, $routeParams) {
     $scope.$on('$viewContentLoaded', function(){
+      
     });
     
-    // ngclick="nextStep"
-    $scope.nextStep = function() {
-      
+    $scope.nextStep = function(step) {
+      alert(step);
     }
   }
 );
