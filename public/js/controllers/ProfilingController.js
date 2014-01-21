a93fce9df9c874875a948fc0b8a3f72ebaece45d
Gregor Swatek <gregor.swatek@gmail.com>
2014-01-21 15:18:39 +0100

    finished animation effekt on profiling, refac course recommendations into partial, fixer course-box styles

--- public/js/controllers/ProfilingController.js
+++ public/js/controllers/ProfilingController.js
@@ -3,9 +3,24 @@ CourseEditor.controller('ProfilingController',
     $scope.$on('$viewContentLoaded', function(){
       
     });
+
+    $scope.selectAnswer = function(e) {
+      var elem = e.currentTarget;
+  
+      $(elem).parent().find('button').removeClass('selected');
+      $(elem).toggleClass('selected');
+    }    
     
-    $scope.nextStep = function(step) {
-      alert(step);
+    $scope.nextStep = function(nextStep, e) {
+      var elem = e.currentTarget,
+          thisStep = $(elem).closest('.step');
+          nextStep = "." + nextStep;
+      
+      $(nextStep).css('left',thisStep.outerWidth());
+      
+      
+      thisStep.animate({'opacity': 0, 'left': -thisStep.outerWidth()},500);
+      $(nextStep).animate({'opacity': 1, 'left': '0px'},500);
     }
   }
 );
